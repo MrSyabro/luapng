babc5abe4204e0c0ad0a562673fb6e988233a948
MrSyabro <syabro1998@gmail.com>
2024-05-31 03:48:29 +0300

    feat: файлы теперь грузятся в массив чисел в формате подходящем для моделей

--- meta/luapng.lua
+++ meta/luapng.lua
@@ -4,27 +4,21 @@
 local M = {}
 
 ---Read data from file and convert HWC to CHW
----@param inputfile any
----@return string data
----@return number height
----@return number width
+---@param inputfile string
+---@return {height: integer, width: integer}|number[] data
 function M.read(inputfile)
 end
 
 ---Read grayscale data from file and convert HWC to CHW
----@param inputfile any
----@return string data
----@return number height
----@return number width
+---@param inputfile string
+---@return {height: integer, width: integer}|number[] data
 function M.read_grayscale(inputfile)
 end
 
 ---Convert CHW to HWC and write data to file
----@param data string данные для записи
----@param height integer
----@param width integer
+---@param data {height: integer, width: integer}|number[]
 ---@param outputfile string имя файла
-function M.write(data, height, width, outputfile)
+function M.write(data, outputfile)
 end
 
 return M
--- src/luapng.c
+++ src/luapng.c
@@ -5,53 +5,10 @@
 #include "lauxlib.h"
 #include "png.h"
 
-/**
- * convert input from HWC format to CHW format
- * \param input A single image. The byte array has length of 3*h*w
- * \param h image height
- * \param w image width
- * \param output A float array. should be freed by caller after use
- * \param output_count Array length of the `output` param
- */
-void hwc_to_chw(const uint8_t* input, size_t h, size_t w, float** output, size_t* output_count) {
-  size_t stride = h * w;
-  *output_count = stride * 3;
-  float* output_data = (float*)malloc(*output_count * sizeof(float));
-  for (size_t i = 0; i != stride; ++i) {
-    for (size_t c = 0; c != 3; ++c) {
-      output_data[c * stride + i] = input[i * 3 + c];
-    }
-  }
-  *output = output_data;
-}
-
-/**
- * convert input from CHW format to HWC format
- * \param input A single image. This float array has length of 3*h*w
- * \param h image height
- * \param w image width
- * \param output A byte array. should be freed by caller after use
- */
-static void chw_to_hwc(const float* input, size_t h, size_t w, uint8_t** output) {
-  size_t stride = h * w;
-  uint8_t* output_data = (uint8_t*)malloc(stride * 3);
-  for (size_t c = 0; c != 3; ++c) {
-    size_t t = c * stride;
-    for (size_t i = 0; i != stride; ++i) {
-      float f = input[t + i];
-      if (f < 0.f || f > 255.0f) f = 0;
-      output_data[i * 3 + c] = (uint8_t)f;
-    }
-  }
-  *output = output_data;
-}
-
 static int l_read_image_file(lua_State *L)
 {
     const char *input_file = luaL_checkstring(L, 1);
     size_t input_data_length;
-    float* out;
-    size_t output_count;
 
     png_image image; /* The control structure used by libpng */
     /* Initialize the 'png_image' structure. */
@@ -70,14 +27,28 @@ static int l_read_image_file(lua_State *L)
     {
         luaL_error(L, "Finish reading image file failed");
     }
-    hwc_to_chw((const uint8_t*)buffer, image.height, image.width, &out, &output_count);
 
-    lua_pushlstring(L, (const char*)out, output_count * sizeof(float));
-    lua_pushinteger(L, image.height);
-    lua_pushinteger(L, image.width);
+    size_t stride = input_data_length / 3;
+
+    lua_createtable(L, input_data_length, 2);
+
+    lua_pushstring(L, "height");
+    lua_pushinteger(L, (lua_Integer)image.height);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "width");
+    lua_pushinteger(L, (lua_Integer)image.width);
+    lua_rawset(L, -3);
+
+    for (size_t i = 0; i != stride; ++i) {
+        for (size_t c = 0; c != 3; ++c) {
+            lua_pushinteger(L, (uint8_t)buffer[i * 3 + c]);
+            lua_rawseti(L, -2, (lua_Integer)(c * stride + i + 1));
+        }
+    }
+
     free(buffer);
-    free(out);
-    return 3;
+    return 1;
 }
 
 static int l_read_grayscale_image_file(lua_State *L)
@@ -96,46 +67,75 @@ static int l_read_grayscale_image_file(lua_State *L)
     input_data_length = PNG_IMAGE_SIZE(image);
 
     char* buffer = calloc(input_data_length, sizeof(char));
-    float* out = calloc(input_data_length, sizeof(float));
     if (png_image_finish_read(&image, NULL /*background*/, buffer, 0 /*row_stride*/, NULL /*colormap*/) == 0)
     {
         luaL_error(L, "Finish reading image file failed");
     }
-    for (size_t i = 0; i < input_data_length; i++) {
-        out[i] = (float)buffer[i];
+
+    lua_createtable(L, input_data_length, 2);
+
+    lua_pushstring(L, "height");
+    lua_pushinteger(L, (lua_Integer)image.height);
+    lua_rawset(L, -3);
+
+    lua_pushstring(L, "width");
+    lua_pushinteger(L, (lua_Integer)image.width);
+    lua_rawset(L, -3);
+
+    for (size_t i = 0; i < input_data_length; ++i) {
+        lua_pushinteger(L, (uint8_t)buffer[i]);
+        lua_rawseti(L, -2, (lua_Integer)(i + 1));
     }
 
-    lua_pushlstring(L, (const char*)out, input_data_length * sizeof(float));
-    lua_pushinteger(L, image.height);
-    lua_pushinteger(L, image.width);
     free(buffer);
-    free(out);
-    return 3;
+    return 1;
 }
 
 static int l_write_image_file(lua_State *L)
 {
-    const char *image_data = luaL_checkstring(L, 1);
-    unsigned int height = (unsigned int)luaL_checkinteger(L, 2);
-    unsigned int width = (unsigned int)luaL_checkinteger(L, 3);
-    const char *output_file = luaL_checkstring(L, 4);
+    luaL_checktype(L, 1, LUA_TTABLE);
+    const char *output_file = luaL_checkstring(L, 2);
 
-    uint8_t *image_data_hwc;
-    chw_to_hwc((const float *)image_data, height, width, &image_data_hwc);
+    lua_getfield(L, 1, "height");
+    if (!lua_isnumber(L, -1)) { luaL_error(L, "Image data height is not a number"); return 0; }
+    size_t h = (size_t)lua_tointeger(L, -1);
+    lua_getfield(L, 1, "width");
+    if (!lua_isnumber(L, -1)) { luaL_error(L, "Image data width is not a number"); return 0; }
+    size_t w = (size_t)lua_tointeger(L, -1);
+    lua_pop(L, 2);
+
+    size_t stride = h * w;
+    uint8_t* output_data = (uint8_t*)malloc(stride * 3);
+    for (size_t c = 0; c != 3; ++c) {
+        size_t t = c * stride;
+        for (size_t i = 0; i != stride; ++i) {
+            lua_rawgeti(L, 1, (lua_Integer)(t + i + 1));
+            if (!lua_isnumber(L, -1)) {
+                luaL_error(L, "Bad element type");
+                free(output_data);
+                return 0;
+            }
+            lua_Number f = lua_tonumber(L, -1);
+            lua_pop(L, 1);
+            if (f < 0.f) f = 0;
+            if (f > 255.0f) f = 255;
+            output_data[i * 3 + c] = (uint8_t)f;
+        }
+    }
 
     png_image image;
     memset(&image, 0, (sizeof image));
     image.version = PNG_IMAGE_VERSION;
     image.format = PNG_FORMAT_BGR;
-    image.height = height;
-    image.width = width;
-    if (png_image_write_to_file(&image, output_file, 0 /*convert_to_8bit*/, image_data_hwc, 0 /*row_stride*/,
+    image.height = h;
+    image.width = w;
+    if (png_image_write_to_file(&image, output_file, 0 /*convert_to_8bit*/, output_data, 0 /*row_stride*/,
                                 NULL /*colormap*/) == 0)
     {
         luaL_error(L, "write to '%s' failed:%s\n", output_file, image.message);
     }
 
-    free(image_data_hwc);
+    free(output_data);
     return 0;
 }
 
